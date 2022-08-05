86da8d83063441bcaf64f2f0947b43ecac986d2a
Kevin Denver <kevin.denver@gmail.com>
2022-08-05 13:16:52 +0100

    DS_Store build gradle-wrapper gradlew settings versions

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+}
+
+group = "com.github.toastshaman"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(Testing.junit.jupiter.api)
+    testRuntimeOnly(Testing.junit.jupiter.engine)
+
+    testImplementation("org.assertj:assertj-core:_")
+    testImplementation("com.amazonaws:aws-lambda-java-events:_")
+    testImplementation("com.amazonaws:aws-lambda-java-core:_")
+    testImplementation("com.google.code.gson:gson:_")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
