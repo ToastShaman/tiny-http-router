fc8673200beff6368d24dac49bc1a14d25bdb158
Kevin Denver <kevin.denver@gmail.com>
2022-08-10 17:22:20 +0100

    build

--- servlet/build.gradle.kts
+++ servlet/build.gradle.kts
@@ -3,5 +3,4 @@ dependencies {
     compileOnly("jakarta.servlet:jakarta.servlet-api:_")
 
     testImplementation("org.eclipse.jetty:jetty-server:_")
-    testImplementation("jakarta.servlet:jakarta.servlet-api:_")
 }
